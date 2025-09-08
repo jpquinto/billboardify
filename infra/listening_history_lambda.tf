3bc5545d727d3531ad3c4edfd6699b866eca9edd
Jeremy Quinto <jpq2901@gmail.com>
2025-09-08 15:11:40 -0700

    Set up listening history ingestion lambda and recent listening dynamodb table

---
+++ infra/listening_history_lambda.tf
@@ -0,0 +1,46 @@
+module "listening_history_ingestor_lambda" {
+  source  = "./modules/lambda"
+  context = module.null_label.context
+
+  name            = "listening-history-ingestor-lambda"
+  handler         = "handler.handler"
+  source_dir      = "${path.root}/../backend/dist/listening_history_ingestor"
+  build_path      = "${path.root}/../backend/build/listening_history_ingestor/listening_history_ingestor.zip"
+  runtime         = "nodejs20.x"
+  memory          = 256
+  time_limit      = 60
+  deployment_type = "zip"
+  zip_project     = true
+
+  layers = [
+    module.shared_lambda_layer.layer_arn,
+  ]
+
+  environment_variables = {
+    REGION : var.aws_region
+    AWS_ACCOUNT_ID : local.account_id
+  }
+}
+
+resource "aws_iam_policy" "listening_history_ingestor_policy" {
+  name        = "listening-history-ingestor-policy"
+  description = "Allows the listening history ingestor Lambda to write to the DynamoDB table."
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "dynamodb:PutItem",
+        ],
+        Resource = module.recent_listening_history_table.arn
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "listening_history_ingestor_attach" {
+  role       = module.listening_history_ingestor_lambda.role_name
+  policy_arn = aws_iam_policy.listening_history_ingestor_policy.arn
+}
